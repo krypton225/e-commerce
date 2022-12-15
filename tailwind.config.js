9da54627dbe0d373b92cd578b4a906e358d32035
Krypton <k.mohamed.11.98@gmail.com>
2022-12-15 14:23:24 +0200

    ADD colors & container @ tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      paddingLeft: "0.5rem",
+      paddingRight: "0.5rem"
+    },
     extend: {},
+    colors: {
+      "dark-3": "#23272a",
+      "dark-2": "#36393e",
+      "dark-1": "#424549",
+      "custom-gray": "#99aab5",
+      "white": "#FFFFFF",
+      "custom-blue": "#7289da",
+    }
   },
   plugins: [],
 }
