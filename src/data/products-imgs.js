c42e44749239ab24e01453969a3d5f508f4d756d
Krypton <k.mohamed.11.98@gmail.com>
2022-12-19 09:00:07 +0200

    ADD products of images

---
+++ src/data/products-imgs.js
@@ -0,0 +1,22 @@
+import insertID from "./generateID";
+
+const images = [
+    { imagePath: "../assets/images/phone-1.png", imageAltText: "Phone 1" },
+    { imagePath: "../assets/images/phone-2.png", imageAltText: "Phone 2" },
+    { imagePath: "../assets/images/phone-3.png", imageAltText: "Phone 3" },
+    { imagePath: "../assets/images/phone-4.png", imageAltText: "Phone 4" },
+    { imagePath: "../assets/images/phone-5.png", imageAltText: "Phone 5" },
+    { imagePath: "../assets/images/phone-6.png", imageAltText: "Phone 6" },
+    { imagePath: "../assets/images/phone-7.png", imageAltText: "Phone 7" },
+    { imagePath: "../assets/images/phone-8.png", imageAltText: "Phone 8" },
+    { imagePath: "../assets/images/phone-9.png", imageAltText: "Phone 9" },
+    { imagePath: "../assets/images/phone-10.png", imageAltText: "Phone 10" },
+    { imagePath: "../assets/images/phone-11.png", imageAltText: "Phone 11" },
+    { imagePath: "../assets/images/phone-12.png", imageAltText: "Phone 12" },
+    { imagePath: "../assets/images/phone-13.png", imageAltText: "Phone 13" },
+    { imagePath: "../assets/images/phone-14.png", imageAltText: "Phone 15" },
+];
+
+(() => insertID(contactData))();
+
+export default images;
