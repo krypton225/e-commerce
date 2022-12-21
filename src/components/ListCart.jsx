e270451bd716195af1c5f6763c80930e412fdaa9
Krypton <k.mohamed.11.98@gmail.com>
2022-12-21 23:55:01 +0200

    ADD back btn @ ListCart component

--- src/components/ListCart.jsx
+++ src/components/ListCart.jsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 
+import { Link } from "react-router-dom";
+
+import { AiOutlineArrowRight } from "react-icons/ai";
+
 import { useRecoilState } from "recoil";
 import { cartAtom } from "../atoms/cartAtom";
 
@@ -13,7 +17,11 @@ const ListCart = () => {
     }, []);
 
     return (
-        <section className="section-padding">
+        <section className="section-padding relative">
+            <Link to={"/"} className="custom-btn w-fit bg-custom-blue border-0 capitalize fixed right-4 bottom-8 z-[999999999]">
+                <AiOutlineArrowRight />
+            </Link>
+
             <SectionTitle innerText="list of cart" styleAdditions="animate-top-bottom" />
 
             <div className="container grid min[400px]:grid-cols-1 max-[639px]:grid-cols-1 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
