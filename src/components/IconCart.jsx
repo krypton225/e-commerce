6f6fe5f40a9aaab81ef91ad36b37502335fb7330
Krypton <k.mohamed.11.98@gmail.com>
2022-12-21 23:41:44 +0200

    ADD check for length items in cart icon @ navbar

--- src/components/IconCart.jsx
+++ src/components/IconCart.jsx
@@ -11,9 +11,13 @@ const IconCart = () => {
     return (
         <Link to={"list"} className="relative">
             <AiOutlineShoppingCart size={24} />
-            <span className="absolute -top-3 -right-6 w-1 h-1 p-3 rounded-full bg-custom-blue flex justify-center items-center text-sm font-semibold">
-                {arr.length}
-            </span>
+            {
+                arr.length === 0 ? "" :
+                    <span className="absolute -top-3 -right-6 w-1 h-1 p-3 rounded-full bg-custom-blue flex justify-center items-center text-sm font-semibold">
+                        {arr.length}
+                    </span>
+            }
+
         </Link>
     )
 }
